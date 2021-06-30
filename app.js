203f38909645a85a911ee95a8a7ea93b58df93de
Antonov Kirill <das511@list.ru>
2021-06-30 16:04:42 +0300

    переписал используя http и  url

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const express = require('express')
-const fs = require('fs');
-const app = express()
+const http = require('http')
+const fs = require('fs')
 const port = 3000
+const url = require('url')
 
-app.get('/get', (req, res) => {
-    fs.readFile("nodeStorage.json", "utf8", (error,data) => {
-        if(error) throw error;
-        res.json(data)
-    })
-})
 
-app.get('/set/:thumbprint', (req, res) => {
-    fs.writeFile("nodeStorage.json", JSON.stringify({thumbprint: req.params.thumbprint}), (error)=>{
+const server = http.createServer((req, res) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private')
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept,, cache-control');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const pathName = url.parse(req.url,true).pathname;
+    const splitter = pathName.split('/');
+    if(splitter.length === 3 && splitter[0] === '' && splitter[1] === 'set') {
+        fs.writeFile("nodeStorage.json", JSON.stringify({thumbprint: splitter[2]}), (error)=>{
         if(error) throw(error) });
-    res.json({state: "writted"}) 
-})
+        res.end(JSON.stringify({state: "writted", value: splitter[2]})) 
+    }
+    else if(splitter.length === 2 && splitter[0] === '' && splitter[1] === 'get'){
+        fs.readFile("nodeStorage.json", "utf8", (error,data) => {
+            if(error) throw error;
+            res.end(JSON.stringify(data))
+        })
+    }
+    else res.end(JSON.stringify({error: "no url"})) 
+}).listen(port);
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+
+
+
+
+
+
